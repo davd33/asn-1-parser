943792bfc8adb4009fc5afbae4963229f7da38ff
David Rueda <davd33@gmail.com>
2019-10-26 13:12:24 +0200

    create app project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to asn-1-parser
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/asn_1_parser/core_test.clj
@@ -0,0 +1,7 @@
+(ns asn-1-parser.core-test
+  (:require [clojure.test :refer :all]
+            [asn-1-parser.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
